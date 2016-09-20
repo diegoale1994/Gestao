88cebab53e72a427dbd4d6b804d5181c5c6dc4b9
Diego <diego.fc.1@hotmail.com>
2016-09-20 14:01:18 -0500

    Segunda grilla

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 	
 	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
+ @if($dia)
 <table class="table table-hover">
     <thead class="thead-inverse">
         <tr>
@@ -53,6 +54,51 @@
         
     </tbody>
 </table>
+@endif
+ @if(!$dia)
+<table class="table table-hover">
+    <thead class="thead-inverse">
+      <tr>
+      <th>#</th>
+      <td>Lunes</td>
+      <td>Martes</td>
+      <td>Miercoles</td>
+      <td>Jueves</td>
+      <td>Viernes</td>
+      <td>Sabado</td> 
+      </tr>
+    </thead>
+    <tbody>
+    @for ($j = 7; $j < 22; $j++)
+        <tr>
+        <th scope="row">{{ $j }} - {{ $j + 1 }} </th>
+       @for ($i = 0; $i < 6; $i++)
+
+  
+<?php $a=0; ?>
+      @foreach ($clases_today as $element)
+     @if (($element -> hora_inicio <= $j) && ($element -> hora_final > $j) && ($element -> fecha == date ("Y-m-d", strtotime("+".$i." day", strtotime($fecha)))))
+       <td>{{ $element -> nombre }}</td>
+     <?php $a++; ?>  
+     @endif
+      @endforeach
+@if ($a == 0)
+  <td>
+<input type="button" value="Reservar" onClick="window.location.href='/admin/reserva/{{ $aula }}/{{ $j }}/{{ date ("Y-m-d", strtotime("+".$i." day", strtotime($fecha))) }}' ">
+
+
+
+  </td>
+@endif
+@endfor
+        </tr>
+            
+            @endfor
+           
+        
+    </tbody>
+</table>
+@endif 
 
 		</div>
 		
