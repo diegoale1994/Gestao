9ad06385e337777c905d0ee293afef6230902247
Diego Alejandro Franco <diego.fc.1@hotmail.com>
2016-10-05 16:51:10 -0500

    show teachers

---
+++ mobile/show_teachers.php
@@ -0,0 +1,23 @@
+
+<?php
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+   
+   require_once('base.php');
+
+    $statement = mysqli_prepare($con, "SELECT  id ,nombre1, apellido1 FROM persona where rol = 'D'");
+   
+    mysqli_stmt_execute($statement);
+    
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement, $id, $nombre1, $apellido1);
+    
+    $response = array();
+    
+    while(mysqli_stmt_fetch($statement)){
+        $response[] = array("id"=>$id,"nombre"=>$nombre1,"apellido"=>$apellido1);
+    }
+    
+    echo json_encode(array('response'=>$response));
+}
+?>
