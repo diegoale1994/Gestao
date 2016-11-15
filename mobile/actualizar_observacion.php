c3d24ddd931f1c931783507815fd1f73af2d5b8c
22edwardr <edwardd_22@hotmail.com>
2016-11-14 22:37:57 -0500

    Observaciones docente

---
+++ mobile/actualizar_observacion.php
@@ -0,0 +1,73 @@
+<?php
+
+
+if($_SERVER["REQUEST_METHOD"]=='POST'){
+
+require_once('base.php');
+
+	$id_clase = $_POST['id_clase'];
+	$fecha = $_POST['fecha'];
+	$observacion=$_POST['observacion'];
+	
+
+	$query = "UPDATE clase_aula_horario SET observaciones=". (($observacion=='')?"NULL":("'".$observacion."'")) ." WHERE id_clase='$id_clase' AND fecha='$fecha' ";
+	
+	$final = mysqli_query($con, $query);
+	if(!$final){
+        echo 'No se pudo actualizar su observacion';
+    }else{
+    	 echo 'Observacion actualizada';
+    }
+
+    if(!$observacion==''){
+    	 $query = "SELECT cell_token, nombre from persona, estudiante_clase, clase where estudiante_clase.id_clase = '$id_clase' and estudiante_clase.id_persona=persona.id and clase.id='$id_clase'";
+     $result = mysqli_query($con, $query);
+    $number_of_rows = mysqli_num_rows($result);
+    
+    $tokens = array();
+    $clase_n="";
+    if($number_of_rows >= 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $tokens[] = $row['cell_token'];
+            $clase_n = $row['nombre'];
+        }
+    }
+
+	$message = array("message" => "Cambio observacion: ".$clase_n." para la fecha ".$fecha);
+	$message_status = send_notification($tokens, $message);
+    }
+	mysqli_close($con);
+	
+}
+
+
+
+function send_notification ($tokens, $message)
+	{
+		$url = 'https://fcm.googleapis.com/fcm/send';
+		$fields = array(
+			 'registration_ids' => $tokens,
+			 'data' => $message
+			);
+		$headers = array(
+			'Authorization:key = AIzaSyBx4558ic1QeIaDgi3_ShbT_M7HtgkPMs8',
+			'Content-Type: application/json'
+			);
+	   $ch = curl_init();
+       curl_setopt($ch, CURLOPT_URL, $url);
+       curl_setopt($ch, CURLOPT_POST, true);
+       curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+       curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);  
+       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+       curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+       $result = curl_exec($ch);           
+       if ($result === FALSE) {
+           die('Curl failed: ' . curl_error($ch));
+       }
+       curl_close($ch);
+       return $result;
+	}
+
+
+?>
--- mobile/select_horario_persona.php
+++ mobile/select_horario_persona.php
@@ -10,20 +10,20 @@ $rol= $_POST["rol"];
    require_once('base.php');
 
 if($rol=="D"){
-    $statement = mysqli_prepare($con,"SELECT fecha,clase.nombre,IFNULL(grupo,'Sin grupo'),hora_inicio,hora_final,IFNULL(observaciones,'Sin observaciones'),IFNULL(aula.nombre,'Sin aula asignada'),IF(id_docente=NULL,'Sin docente asignado',CONCAT(persona.nombre1,' ',persona.apellido1)) FROM clase JOIN clase_aula_horario ON id_clase=clase.id LEFT JOIN aula ON ((id_aula=aula.id)OR(id_aula=NULL)) LEFT JOIN persona ON ((id_docente=persona.id)OR(id_docente=NULL)) WHERE id_docente=? AND fecha=? ");
+    $statement = mysqli_prepare($con,"SELECT clase.id,fecha,clase.nombre,IFNULL(grupo,'Sin grupo'),hora_inicio,hora_final,IFNULL(observaciones,'Sin observaciones'),IFNULL(aula.nombre,'Sin aula asignada'),IF(id_docente=NULL,'Sin docente asignado',CONCAT(persona.nombre1,' ',persona.apellido1)) FROM clase JOIN clase_aula_horario ON id_clase=clase.id LEFT JOIN aula ON ((id_aula=aula.id)OR(id_aula=NULL)) LEFT JOIN persona ON ((id_docente=persona.id)OR(id_docente=NULL)) WHERE id_docente=? AND fecha=? ");
 }else{
-    $statement = mysqli_prepare($con,"SELECT fecha,clase.nombre,IFNULL(grupo,'Sin grupo'),hora_inicio,hora_final,IFNULL(observaciones,'Sin observaciones'),IFNULL(aula.nombre,'Sin aula asignada'),IF(id_docente=NULL,'Sin docente asignado',CONCAT(persona.nombre1,' ',persona.apellido1)) FROM clase JOIN clase_aula_horario ON clase_aula_horario.id_clase=clase.id LEFT JOIN aula ON ((id_aula=aula.id)OR(id_aula=NULL)) JOIN estudiante_clase ON ((estudiante_clase.id_persona= ? ) AND (estudiante_clase.id_clase=clase_aula_horario.id_clase)) LEFT JOIN persona ON ((id_docente=persona.id)OR(id_docente=NULL)) WHERE fecha= ? ");
+    $statement = mysqli_prepare($con,"SELECT clase.id,fecha,clase.nombre,IFNULL(grupo,'Sin grupo'),hora_inicio,hora_final,IFNULL(observaciones,'Sin observaciones'),IFNULL(aula.nombre,'Sin aula asignada'),IF(id_docente=NULL,'Sin docente asignado',CONCAT(persona.nombre1,' ',persona.apellido1)) FROM clase JOIN clase_aula_horario ON clase_aula_horario.id_clase=clase.id LEFT JOIN aula ON ((id_aula=aula.id)OR(id_aula=NULL)) JOIN estudiante_clase ON ((estudiante_clase.id_persona= ? ) AND (estudiante_clase.id_clase=clase_aula_horario.id_clase)) LEFT JOIN persona ON ((id_docente=persona.id)OR(id_docente=NULL)) WHERE fecha= ? ");
 }
     mysqli_stmt_bind_param($statement, "is", $id_persona,$fecha);
     mysqli_stmt_execute($statement);
     
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $fecha,$clase_nombre,$grupo,$hora_inicio,$hora_final,$observaciones,$aula_nombre,$docente);
+    mysqli_stmt_bind_result($statement, $claseId,$fecha,$clase_nombre,$grupo,$hora_inicio,$hora_final,$observaciones,$aula_nombre,$docente);
     
     $response = array();
     
     while(mysqli_stmt_fetch($statement)){
-        $response[] = array("fecha"=>$fecha,"clase"=>$clase_nombre,"grupo"=>$grupo,"hora_inicio"=>$hora_inicio,"hora_final"=>$hora_final,"observaciones"=>$observaciones,"aula"=>$aula_nombre,"docente"=>$docente);
+        $response[] = array("clase_id"=>$claseId,"fecha"=>$fecha,"clase"=>$clase_nombre,"grupo"=>$grupo,"hora_inicio"=>$hora_inicio,"hora_final"=>$hora_final,"observaciones"=>$observaciones,"aula"=>$aula_nombre,"docente"=>$docente);
     }
     
     echo json_encode(array('response'=>$response));
